b28c1c1b52e801f313bcbcedfaa0b8b711540b1b
timmy625 <tjmtjm625@gmail.com>
2021-11-20 12:24:05 -0500

    Update dec3.cpp

--- USACO Silver/2016 - 2017/dec3.cpp
+++ USACO Silver/2016 - 2017/dec3.cpp
@@ -1,12 +1,11 @@
+//Timothy Mathew (timmy625)
 #include <bits/stdc++.h>
 using namespace std;
 
 #define pb push_back
-#define mp make_pair
 #define fi first
 #define se second
-#define sz(x) (int)(x).size()
-#define FOR(i, a, b) for(long long i = a; i < b; i++)
+#define FOR(i, a, b) for(int i = a; i < b; i++)
 #define odd(x) ((x) % (2))
 #define fastio ios_base::sync_with_stdio(false), cin.tie(0);
 #define fileio freopen("moocast.in", "r", stdin), freopen("moocast.out", "w", stdout);
@@ -15,43 +14,42 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 const ll MOD = 1e9 + 7;
 
-struct cow {int x, y, p;};
+struct Cow {int x, y, p;};
 vi adj[205];
-bool visited[205];
-
-int dfs(int u, int total) {
-    visited[u] = true;
-    total++;
+bool vis[205];
+int dfs(int u, int sz) {
+    vis[u] = true;
     for (int v : adj[u]) {
-        if (!visited[v]) {total = max(total, dfs(v, total));}
+        if (!vis[v]) {sz += dfs(v, 1);}
     }
-    return total;
+    return sz;
 }
-
 int main() {
     fastio;
     fileio;
     int n;
     cin >> n;
-    cow cows[n];
+    Cow cows[n];
     FOR(i, 0, n) {
         cin >> cows[i].x >> cows[i].y >> cows[i].p;
     }
     FOR(i, 0, n) {
-        FOR(j, 0, n) {
-            if (i == j) {continue;}
-            int distX = abs(cows[i].x - cows[j].x) * abs(cows[i].x - cows[j].x);
-            int distY = abs(cows[i].y - cows[j].y) * abs(cows[i].y - cows[j].y);
-            int power = cows[i].p * cows[i].p;
-            if (distX + distY < power) {
+        FOR(j, i+1, n) {
+            int distX = cows[i].x - cows[j].x;
+            int distY = cows[i].y - cows[j].y;
+            int totalDist = distX*distX + distY*distY;
+            if (totalDist <= cows[i].p*cows[i].p) {
                 adj[i].pb(j);
+            }
+            if (totalDist <= cows[j].p*cows[j].p) {
+                adj[j].pb(i);
             }
         }
     }
     int best = 0;
     FOR(i, 0, n) {
-        FOR(j, 0, n) {visited[j] = false;}
-        best = max(best, dfs(i, 0));
+        FOR(j, 0, n) {vis[j] = false;}
+        best = max(best, dfs(i, 1));
     }
     cout << best << "\n";
     return 0;
